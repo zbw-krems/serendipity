3498650621eb544afdcdad8c637f1d2283b39bf2
Fabian Schober <reach@fabianschober.com>
2023-12-13 11:07:31 +0100

    OR label between FilterChains

--- src/Components/Filters/FilterChain.js
+++ src/Components/Filters/FilterChain.js
@@ -55,93 +55,97 @@ export const FilterChain = ({
   // console.log(graphContext.graphState);
 
   return (
-    <div
-      className="list-group-item "
-      key={`FilterChain-Wrapper-${filterChain.uid}`}
-      style={{ backgroundColor: filterChain.color }}
-    >
-      {filterChain.uid ? <div className="badge bg-black">OR</div> : <></>}
-      <h5 className="text-white" key={`FilterChain-h5-${filterChain.uid}`}>
-        {filterChain.uid != 0 ? (
-          <LightTooltip title="Delete Filterchain">
-            <DeleteIcon
-              fontSize={"small"}
-              onClick={() => handleDeleteFilterCain(filterChain.uid)}
+    <>
+      <div>
+        {filterChain.uid ? <div className="badge bg-black m-1">OR</div> : <></>}
+      </div>
+      <div
+        className="list-group-item "
+        key={`FilterChain-Wrapper-${filterChain.uid}`}
+        style={{ backgroundColor: filterChain.color }}
+      >
+        <h5 className="text-white" key={`FilterChain-h5-${filterChain.uid}`}>
+          {filterChain.uid != 0 ? (
+            <LightTooltip title="Delete Filterchain">
+              <DeleteIcon
+                fontSize={"small"}
+                onClick={() => handleDeleteFilterCain(filterChain.uid)}
+              />
+            </LightTooltip>
+          ) : (
+            <></>
+          )}
+          <EditIcon
+            fontSize={"small"}
+            sx={{ paddingX: "2px", marginX: "3px" }}
+            onClick={() => setIsEditing(!isEditing)}
+          />
+          <LightTooltip title="Mute FilterChain">
+            <FilterAltOffIcon
+              fontSize={"smaller"}
+              sx={{ paddingX: "2px", marginX: "3px" }}
+              onClick={() => {
+                handleMute();
+              }}
             />
           </LightTooltip>
-        ) : (
-          <></>
-        )}
-        <EditIcon
-          fontSize={"small"}
-          sx={{ paddingX: "2px", marginX: "3px" }}
-          onClick={() => setIsEditing(!isEditing)}
-        />
-        <LightTooltip title="Mute FilterChain">
-          <FilterAltOffIcon
-            fontSize={"smaller"}
-            sx={{ paddingX: "2px", marginX: "3px" }}
-            onClick={() => {
-              handleMute();
-            }}
-          />
-        </LightTooltip>
-        {isEditing ? (
-          <form
-            className="renameForm d-flex align-items-end"
-            onSubmit={(e) => {
-              // console.log(e);
-              return handleEditFilterChainTitle(e, filterChain);
-            }}
-          >
-            <input
-              type="text"
-              ref={TextRef}
-              value={filterChain.title}
-              className="renameInput"
-            />
-            <button type="submit" className="renameSubmitButton">
-              <CheckIcon fontSize="smaller" sx={{ paddingBottom: "5px" }} />
-            </button>
-          </form>
-        ) : (
-          filterChain.title + " "
-        )}
-        ({filterChain.keywords.length}/{KEYWORDSINFILTERCHAINSLIMIT})
-      </h5>
-      <p>
-        <i className="text-white small">
-          {graphContext.graphState.graphData.nodes.length + " "}
-          Artworks
-        </i>
-      </p>
+          {isEditing ? (
+            <form
+              className="renameForm d-flex align-items-end"
+              onSubmit={(e) => {
+                // console.log(e);
+                return handleEditFilterChainTitle(e, filterChain);
+              }}
+            >
+              <input
+                type="text"
+                ref={TextRef}
+                value={filterChain.title}
+                className="renameInput"
+              />
+              <button type="submit" className="renameSubmitButton">
+                <CheckIcon fontSize="smaller" sx={{ paddingBottom: "5px" }} />
+              </button>
+            </form>
+          ) : (
+            filterChain.title + " "
+          )}
+          ({filterChain.keywords.length}/{KEYWORDSINFILTERCHAINSLIMIT})
+        </h5>
+        <p>
+          <i className="text-white small">
+            {graphContext.graphState.graphData.nodes.length + " "}
+            Artworks
+          </i>
+        </p>
 
-      {!filterChain.keywords.length && (
-        <small className="text-muted">No Keywords in Filter yet</small>
-      )}
-      <div
-        className="list-group text-right filtersItem filterChainScroll"
-        key={`filterChainGroup-${filterChain.uid}`}
-      >
-        {filterChain.keywords.map((keyword) => {
-          return (
-            <KeywordInFilterChain
-              key={`KeywordInFilterChainComponent-${filterChain.uid}-${keyword.uid}`}
-              keyword={keyword}
-              filterChain={filterChain}
-              filterContext={filterContext}
-              deleteKeywordFromFilterChain={deleteKeywordFromFilterChain}
-              moveKeywordToFilterChain={moveKeywordToFilterChain}
-              cloneKeywordToFilterChain={cloneKeywordToFilterChain}
-              addAllRelatedKeywordsToFilterChain={
-                addAllRelatedKeywordsToFilterChain
-              }
-              handleCreateFilterChain={handleCreateFilterChain}
-              handleCreateUserCollection={handleCreateUserCollection}
-            />
-          );
-        })}
+        {!filterChain.keywords.length && (
+          <small className="text-muted">No Keywords in Filter yet</small>
+        )}
+        <div
+          className="list-group text-right filtersItem filterChainScroll"
+          key={`filterChainGroup-${filterChain.uid}`}
+        >
+          {filterChain.keywords.map((keyword) => {
+            return (
+              <KeywordInFilterChain
+                key={`KeywordInFilterChainComponent-${filterChain.uid}-${keyword.uid}`}
+                keyword={keyword}
+                filterChain={filterChain}
+                filterContext={filterContext}
+                deleteKeywordFromFilterChain={deleteKeywordFromFilterChain}
+                moveKeywordToFilterChain={moveKeywordToFilterChain}
+                cloneKeywordToFilterChain={cloneKeywordToFilterChain}
+                addAllRelatedKeywordsToFilterChain={
+                  addAllRelatedKeywordsToFilterChain
+                }
+                handleCreateFilterChain={handleCreateFilterChain}
+                handleCreateUserCollection={handleCreateUserCollection}
+              />
+            );
+          })}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
